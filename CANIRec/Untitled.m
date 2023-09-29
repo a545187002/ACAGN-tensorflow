af528629970bf56abb09870d45732a9b063d0cbe
a545187002 <59232087+a545187002@users.noreply.github.com>
2023-09-29 10:16:58 +0800

    Delete CANIRec directory

--- CANIRec/Untitled.m
+++
@@ -1,5 +0,0 @@
-file_names = dir('*.csv'); %读取所有的.csv文件
-for i = 1: numel(file_names)  
-    data = csvread(file_names(i).name); %依次读取csv格式的数据
-    save([num2str(i),'.mat'],'data');  % save(文件名, 变量)，依次存储为mat格式
-end 
